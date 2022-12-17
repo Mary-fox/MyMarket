7fd988e2a2d844e4763f321b715f0280405df4c3
Mary <89971822+Mary-fox@users.noreply.github.com>
2022-12-17 18:44:43 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import {BrowserRouter,Routes,Route} from"react-router-dom";
+
 import PageProduct from './components/PageProduct/PageProduct';
-import MainPage from "./components/MainPage/MainPage";
+
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage/>}/>
-        <Route path="/product" element={<PageProduct/>}/>
-      </Routes>
-    </BrowserRouter>
+        <PageProduct />
   );
 }
 
