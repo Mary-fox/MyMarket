002809f33614e86e02766c772d659d634c1bcbee
Mary <89971822+Mary-fox@users.noreply.github.com>
2022-12-17 19:03:08 +0300

    feat: add change in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import {HashRouter,Routes,Route} from"react-router-dom";
 import PageProduct from './components/PageProduct/PageProduct';
-
+import MainPage from "./components/MainPage/MainPage";
 
 
 function App() {
   return (
-        <PageProduct />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>}/>
+        <Route path="/product" element={<PageProduct/>}/>
+      </Routes>
+    </HashRouter>
   );
 }
 
